d5a608b9a710a78bda4639e59c9663fdba15867e
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-01 14:57:41 -0300

    Criação da classe RegisterServlet

---
+++ src/main/java/br/edu/dsw1/RegisterSevlet.java
@@ -0,0 +1,19 @@
+package br.edu.dsw1;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register.do")
+public class RegisterSevlet extends HttpServlet {
+	private final static long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+}
