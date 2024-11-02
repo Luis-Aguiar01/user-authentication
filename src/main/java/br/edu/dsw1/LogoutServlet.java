696e467527f28fa40402f779523987f8e5e04eb9
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-11-02 20:11:25 -0300

    Término da implementação do Servlet de logout

---
+++ src/main/java/br/edu/dsw1/LogoutServlet.java
@@ -0,0 +1,21 @@
+package br.edu.dsw1;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout.do")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		var session = request.getSession(false);
+		session.invalidate();
+		response.sendRedirect("login.jsp");
+	}
+}
